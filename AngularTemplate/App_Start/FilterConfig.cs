a4023609eb8e5e34f77f2cb540a8c91dc224ec44
bachmeyer <wesyb@yahoo.com>
2016-01-19 20:46:21 -0600

    initial upload of angular template

---
+++ AngularTemplate/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AngularTemplate {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AngularTemplate/ngApp/app.ts
@@ -0,0 +1,12 @@
+﻿namespace $safeprojectname$ {
+
+    angular.module("$safeprojectname$", ['ngRoute'])
+        .config(function($routeProvider) {
+            //$routeProvider.when('/', {
+            // templateUrl: '/path/to/view',
+            // controller: $safeprojectname$.Controllers.ControllerClass,
+            // controllerAs: 'views variable name for controller'
+            //});
+            $routeProvider.when('/', { template: 'Hello World!' });
+        });
+}
